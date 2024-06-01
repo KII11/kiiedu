d087bd0d50537aaa274628c3c35493cc7a05705b
riskifer <145009421+KII11@users.noreply.github.com>
2024-06-02 05:21:58 +0700

    w

--- scripts.js
+++ scripts.js
@@ -1,24 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var toggle = document.getElementById('toggle-menu');
     var nav = document.getElementById('mobile-nav');
-
-    if (!toggle || !nav) {
-        console.error('Navigation elements are missing on this page.');
-        return;
-    }
-
-    // Inisialisasi posisi awal navigasi dan tombol
-    nav.style.left = '-50%'; // Menyembunyikan navigasi
-    toggle.style.left = '10px'; // Menempatkan tombol di posisi awal
+    var toggle = document.getElementById('toggle-menu');
+    // Inisialisasi posisi awal jika belum diatur
+    nav.style.left = nav.style.left || '-50%'; 
+    toggle.style.left = toggle.style.left || '10px';
 
     toggle.addEventListener('click', function() {
-        // Cek apakah navigasi terbuka dan tutup/gulir sesuai
         if (nav.style.left === '0px') {
-            nav.style.left = '-50%'; // Geser navigasi keluar
-            toggle.style.left = '10px'; // Kembalikan tombol ke posisi awal
+            nav.style.left = '-50%';
+            toggle.style.left = '10px';
         } else {
-            nav.style.left = '0px'; // Geser navigasi masuk
-            toggle.style.left = 'calc(50% + 10px)'; // Geser tombol menu ke kanan
+            nav.style.left = '0px';
+            toggle.style.left = 'calc(50% + 10px)';
         }
     });
 });
